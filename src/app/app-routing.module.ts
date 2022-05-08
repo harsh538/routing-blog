7d402cddbca995d1c69c6d5dc30fe16a73ac0255
harsh538 <harshadvele538@gmail.com>
2022-05-08 12:22:59 +0530

    Routing Blo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path:'user',
+    component:UserComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
